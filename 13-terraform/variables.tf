f47c08b83e4c686a10ac08d68dc1c0fb9ad28590
Stanisz1 <34337276+Stanisz1@users.noreply.github.com>
2023-05-14 23:15:51 +0200

    Update 13-terraform/variables.tf

--- 13-terraform/variables.tf
+++ 13-terraform/variables.tf
@@ -1,15 +1,34 @@
-variable "tf_token" {}
+variable "deployment" {
+  description = "Deployment-related variables"
+  default = {
+    app_name        = "wcg"
+    name            = "wcg"
+    replica_number  = 1
+    container_image = "ghcr.io/stanisz1/wcg:ace07b74b63cab357e1623f20a19ccf99b6ad187"
+    container_name  = "wcg"
+  }
+}
 
-variable "files" {
-  default = [
-    "deployment.tf",
-    "service.tf",
-    "ingress.tf",
-    "provider.tf",
-    "locals.tf",
-    "provider.tf",
-    "github/provider.tf",
-    "github/repo.tf",
-    "github/variables.tf"
-  ]
+variable "service" {
+  description = "Service-related variables"
+  default = {
+    name        = "wcgservice"
+    port        = 8888
+    target_port = 8888
+  }
+}
+
+variable "ingress" {
+  description = "Ingress-related variables"
+  default = {
+    name      = "wcgingress"
+    host      = "wcgterraformhw.com"
+    port      = 8888
+    path      = "/"
+    path_type = "Prefix"
+  }
+}
+
+variable "lables_app_name" {
+  default = "wcg"
 }
