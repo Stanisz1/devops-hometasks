bd7b7e3e40ea00ef9d2d0df6b8c718d38cba4571
Stanisz1 <34337276+Stanisz1@users.noreply.github.com>
2023-05-14 23:11:17 +0200

    Update 13-terraform/provider.tf

--- 13-terraform/provider.tf
+++ 13-terraform/provider.tf
@@ -1,18 +1,12 @@
 terraform {
   required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.20.0"
+    github = {
+      source  = "integrations/github"
+      version = "5.25.0"
     }
-  }
-  backend "kubernetes" {
-    secret_suffix = "state"
-    config_path   = "~/.kube/config"
-    namespace     = "kube-system"
   }
 }
 
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "minikube"
+provider "github" {
+  token = var.tf_token
 }
